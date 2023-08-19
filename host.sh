fea2db07367950de26e886c6d2452849e6533def
Andrei Shevchuk <andrei@shevchuk.co>
2023-08-19 12:36:49 +0300

    Update host.sh

--- host.sh
+++ host.sh
@@ -4,17 +4,24 @@ set -euo pipefail
 
 dest_dir="$HOME"
 win_dest_dir="/mnt/c/Users/Public/Downloads"
-dockerfile="https://gitlab.com/hadzhioglu/padavan-ng/raw/master/Dockerfile"
-build_script=""
+
+repo_url="${1:-https://gitlab.com/hadzhioglu/padavan-ng}"
+branch="${2:-master}"
+
+dockerfile_url="${repo_url}/raw/${branch}/Dockerfile"
+build_script_url="https://github.com/shvchk/padavan-builder/raw/main/container.sh"
+
+dockerfile="$(wget -qO- "$dockerfile_url")"
+dockerfile+=$'\nRUN wget -qO /opt/container.sh "'$build_script_url'"'
 
 sudo apt update
 sudo apt install podman -y
 
-wget -qO- "$dockerfile"   | podman build    --ulimit nofile=9000 -t padavan -
-wget -qO- "$build_script" | podman run --rm --ulimit nofile=9000 -i -v "$dest_dir":/tmp/trx padavan
+podman build    --ulimit nofile=9000 -t padavan - <<< "$dockerfile"
+podman run --rm --ulimit nofile=9000 -it -v "$dest_dir":/tmp/trx padavan bash /opt/container.sh < /dev/tty
 
 # if we are in WSL, move trx files to $win_dest_dir
 if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop && -w $win_dest_dir ]]; then
-  mkdir "${win_dest_dir}/padavan"
+  mkdir "${win_dest_dir}/padavan" || :
   mv "$dest_dir"/*trx "${win_dest_dir}/padavan"
 fi
