cbae35d27cd42cd8a0a1c73e4553c6757e4832ae
Andrei Shevchuk <andrei@shevchuk.co>
2023-10-02 14:50:24 +0300

    Rewrite
    - Use precompiled toolchain
    - Use compressed disk image

--- host.sh
+++ host.sh
@@ -3,11 +3,16 @@
 set -euo pipefail
 
 dest_dir="$HOME"
-win_dest_dir="/mnt/c/Users/Public/Downloads"
-repo_url="${1:-${PADAVAN_REPO:-https://gitlab.com/hadzhioglu/padavan-ng}}"
+win_dest_dir="/mnt/c/Users/Public/Downloads/padavan"
+repo_url="${1:-${PADAVAN_REPO:-https://gitlab.com/a-shevchuk/padavan-ng}}"
 branch="${2:-${PADAVAN_BRANCH:-master}}"
-containerfile="${PADAVAN_CONTAINERFILE:-${repo_url}/raw/${branch}/Dockerfile}"
-image_name="padavan-builder"
+img="registry.gitlab.com/a-shevchuk/padavan-ng"
+img_name="padavan-builder"
+container="padavan-builder"
+disk_img="${container}.btrfs"
+shared_dir="shared"
+toolchain_url="${repo_url}/-/jobs/artifacts/master/raw/toolchain.tzst?job=build+toolchain"
+
 
 # text decoration utilities
 normal=$(tput sgr0)
@@ -17,7 +22,7 @@ warn_msg="$(tput setab 220; tput setaf 16)${bold}" # yellow bg, black text
 accent="$(tput setab 238; tput setaf 231)${bold}" # gray bg, white text
 
 tmp_dir="$(mktemp -d)"
-log_file="${tmp_dir}/padavan-builder.log"
+log_file="${tmp_dir}/${container}.log"
 log_follow_reminder=" You can follow the log live in another terminal with ${accent} tail -f '$log_file' "
 
 
@@ -29,7 +34,13 @@ _echo() {
 }
 
 _log() {
-  echo -e "$(date +'%Y.%m.%d %H.%M.%S') - ${*}" &>> "$log_file"
+  type=${1}_msg; shift
+  echo -e "$(date +'%Y.%m.%d %H:%M:%S') - $*" &>> "$log_file"
+
+  case "$type" in
+    raw*)         _echo "\n $*" ;;&
+    info*|warn*)  _echo "\n${!type} $* " ;;
+  esac
 }
 
 _handle_exit() {
@@ -38,14 +49,21 @@ _handle_exit() {
     _echo " Failed command: $BASH_COMMAND"
   fi
 
-  podman rm -f "$image_name" &>> "$log_file"
+  _log warn "Cleaning"
+  podman stop "$container" &>> "$log_file"
+
+  _echo "Password required to unmount the disk image"
+  sudo umount "$shared_dir" &>> "$log_file" || :
+
+  _echo "If you don't plan to reuse sources, it's ok to delete disk image"
+  _confirm "Delete $disk_img disk image?" && rm -rf "$disk_img" "$shared_dir" &>> "$log_file"
 }
 
 _confirm() {
   echo
   while true; do
-    # redirecting < /dev/tty is required to be able to run via pipe: cat x.sh | bash
-    read -p "$* [ + or - ]: " confirmation < /dev/tty || { echo "No tty"; exit 1; }
+    # `< /dev/tty` is required to be able to run via pipe: cat x.sh | bash
+    read -rp "$* [ + or - ]: " confirmation < /dev/tty || { echo "No tty"; exit 1; }
     case "$confirmation" in
       "+") return 0 ;;
       "-") return 1 ;;
@@ -57,43 +75,64 @@ _confirm() {
 # main functions
 
 _prepare() {
-  echo "$(date +'%Y.%m.%d %H.%M.%S') - Starting" > "$log_file"
+  echo "$(date +'%Y.%m.%d %H:%M:%S') - Starting" > "$log_file"
   _echo "\n${info_msg} Log file: ${normal}${bold} ${log_file}"
   _echo "$log_follow_reminder"
 
-  _echo "\n${info_msg} Installing podman and utilities "
-  _log  "Installing podman and utilities"
+  _log info "Installing podman and utilities"
   sudo apt update &>> "$log_file"
-  sudo apt install fzf micro podman -y &>> "$log_file"
+  sudo apt install btrfs-progs fzf micro podman zstd -y &>> "$log_file"
+
+  export STORAGE_DRIVER="overlay"
+  export STORAGE_OPTS="overlay.mountopt=volatile"
+
+  ulimit -Sn "$(ulimit -Hn)"
+  if (( $(ulimit -Sn) < 4096 )); then
+    _log warn "Limit on open files: $(ulimit -Sn). Sometimes that is not enough to build the toolchain"
+  fi
+
+  mkdir -p "$shared_dir"
+
+  if [[ -f $disk_img ]]; then
+    _log raw "Existing $disk_img found, using it"
+  else
+    truncate -s 50G "$disk_img"
+    mkfs.btrfs "$disk_img" &>> "$log_file"
+  fi
+
+  sudo mount -o noatime,compress=zstd "$disk_img" "$shared_dir"
+  sudo chown -R $USER:$USER "$shared_dir"
 }
 
-_build_image() {
-  _echo "\n${info_msg} Creating a container image and building the toolchain "
-  _log  "Creating a container image and building the toolchain"
-  _echo " This will take a while, usually 20-60 minutes"
-  _echo "$log_follow_reminder"
-  podman rmi -f "$image_name"
-  podman build --ulimit nofile=9000 --squash -t "$image_name" "$containerfile" &>> "$log_file"
-  _echo " Done"
-  _log  " Done"
+ctnr_exec() {
+  work_dir=$1; shift
+  podman exec -w "$work_dir" "$container" "$@" &>> "$log_file"
 }
 
 _start_container() {
-  _echo "\n${info_msg} Starting container to build firmware "
-  _log  "Starting container to build firmware"
-  podman run --ulimit nofile=9000 -dt -v "$dest_dir":/tmp/trx -w /opt/padavan-ng/trunk --name "$image_name" "$image_name" &>> "$log_file"
+  _log info "Starting container to build firmware"
+  podman run --rm --pull newer -dt -v "$(realpath "$shared_dir")":/opt --name "$container" "$img" &>> "$log_file"
+}
+
+_clone_repo_sparse() {
+  _log info "Cloning $repo_url ($branch)"
+  # sparse checkout
+  ctnr_exec '' git clone -vn --depth 1 --filter tree:0 -b "$branch" "$repo_url" &>> "$log_file"
+  ctnr_exec /opt/padavan-ng git sparse-checkout set --no-cone "$@" &>> "$log_file"
+  ctnr_exec /opt/padavan-ng git checkout &>> "$log_file"
 }
 
 _prepare_build_config() {
+  build_config="${tmp_dir}/padavan-build.config"
   config_selection_header=$(printf "%s\n" "${warn_msg} Select your router model ${normal}" \
                                           " Filter by entering text" \
                                           " Select by mouse or arrow keys" \
                                           " Double click or Enter to confirm")
 
-  config_file="$(podman exec "$image_name" sh -c "find configs/templates/*/*config" | \
-                fzf +m -e -d / --with-nth 3.. --reverse --no-info --bind=esc:ignore --header-first --header "$config_selection_header")"
+  config_file="$(find "${shared_dir}"/padavan-ng/trunk/configs/templates/*/*config | \
+                fzf +m -e -d / --with-nth 6.. --reverse --no-info --bind=esc:ignore --header-first --header "$config_selection_header")"
 
-  podman cp "$image_name":"$config_file" "${tmp_dir}/padavan-build.config"
+  cp "$config_file" "$build_config"
 
   _echo "\n${warn_msg} Edit the build config file "
   _echo
@@ -120,32 +159,30 @@ _prepare_build_config() {
   _echo
   read -rsn1 -p "${accent} Press any key to start the config editor ${normal}" < /dev/tty; echo
 
-  micro -autosave 1 -ignorecase 1 -keymenu 1 -scrollbar 1 -filetype shell "${tmp_dir}/padavan-build.config"
-  podman cp "${tmp_dir}/padavan-build.config" "$image_name":.config
+  micro -autosave 1 -ignorecase 1 -keymenu 1 -scrollbar 1 -filetype shell "$build_config"
+  cp "$build_config" "${shared_dir}/padavan-ng/trunk/.config"
 
-  _echo " Build config backup: ${tmp_dir}/padavan-build.config"
+  _echo " Build config backup: $build_config"
 }
 
 _build_firmware() {
-  _echo "\n${info_msg} Building firmware "
-  _log  "Building firmware"
+  _log info "Building firmware"
   _echo " This will take a while, usually 10-30 minutes"
   _echo "$log_follow_reminder"
-  podman exec "$image_name" ./clear_tree.sh &>> "$log_file"
-  podman exec "$image_name" ./build_firmware.sh &>> "$log_file"
-  _echo " Done"
-  _log  " Done"
+  ctnr_exec /opt/padavan-ng/trunk ./clear_tree.sh &>> "$log_file"
+  ctnr_exec /opt/padavan-ng/trunk ./build_firmware.sh &>> "$log_file"
+  _log raw "Done"
 }
 
 _copy_firmware_to_host() {
   _echo " Copying firmware to $dest_dir"
-  podman exec "$image_name" sh -c "cp images/*trx /tmp/trx"
+  cp "${shared_dir}"/padavan-ng/trunk/images/*trx "$dest_dir"
 
   # if we are in WSL, move trx files to $win_dest_dir
   if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop && -w $win_dest_dir ]]; then
     _echo " Moving firmware to ${win_dest_dir}/padavan"
-    mkdir "${win_dest_dir}/padavan" &> /dev/null || :
-    mv "$dest_dir"/*trx "${win_dest_dir}/padavan"
+    mkdir -p "$win_dest_dir" &> /dev/null || :
+    mv "$dest_dir"/*trx "$win_dest_dir"
   fi
 }
 
@@ -156,17 +193,15 @@ trap _handle_exit EXIT
 
 _prepare
 
-if podman image exists "$image_name"; then
-  _echo "\n${warn_msg} $image_name image already exists "
-  _confirm "Use existing image (+) or delete it and build a new one (-)?" || _build_image
-else
-  _build_image
-fi
-
 _start_container
+_clone_repo_sparse /trunk
+
+# get prebuilt toolchain
+#ctnr_exec '' cp -r /tmp/toolchain padavan-ng
+wget -qO- "$toolchain_url" | tar -C "${shared_dir}/padavan-ng" --zstd -xf -
+
 _prepare_build_config
 _build_firmware
 _copy_firmware_to_host
 
-_echo "\n${info_msg} All done "
-_log  " All done"
+_log info "All done"
